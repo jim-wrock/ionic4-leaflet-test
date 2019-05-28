0e97305ac95c84f4e35e263316da24b79fca9e15
Jim Decocq <jim.decocq@gmail.com>
2019-05-28 10:57:56 +0200

    geojson added in tab1

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,31 +1,43 @@
-import { Component } from '@angular/core';
-import { Map, latLng, tileLayer, Layer, marker } from 'leaflet';
+import { Component } from "@angular/core";
+import { Map, latLng, tileLayer, Layer, marker, geoJSON, GeoJSON } from "leaflet";
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+	selector: "app-tab1",
+	templateUrl: "tab1.page.html",
+	styleUrls: ["tab1.page.scss"],
 })
 export class Tab1Page {
-  map: Map;
+	map: Map;
+	propertyList: GeoJSON.FeatureCollection<any>;
 
-  ionViewDidEnter() { this.leafletMap(); }
+	ionViewDidEnter() {
+		this.leafletMap();
+	}
 
-  leafletMap() {
-    // In setView add latLng and zoom
-    this.map = new Map('mapId').setView([28.644800, 77.216721], 10);
-    tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
-      attribution: 'edupala.com © ionic LeafLet',
-    }).addTo(this.map);
+	leafletMap() {
+		// In setView add latLng and zoom
+		this.map = new Map("mapId").setView([0, 100], 3);
 
+		tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+			attribution: "edupala.com © ionic LeafLet",
+		}).addTo(this.map);
 
-    marker([28.6, 77]).addTo(this.map)
-      .bindPopup('Ionic 4 <br> Leaflet.')
-      .openPopup();
-  }
+		fetch("/assets/data.json")
+			.then(res => res.json())
+			.then(json => {
+				console.log(json);
+				this.propertyList = json;
+				geoJSON(this.propertyList).addTo(this.map);
+			});
 
-  /** Remove map when we have multiple map object */
-  ionViewWillLeave() {
-    this.map.remove();
-  }
+		marker([28.6, 77])
+			.addTo(this.map)
+			.bindPopup("Ionic 4 <br> Leaflet.")
+			.openPopup();
+	}
+
+	/** Remove map when we have multiple map object */
+	ionViewWillLeave() {
+		this.map.remove();
+	}
 }
